1df32d2909122c6a4922b3e933c4f767e1c84852
Diyatt <120357743+Diyatt@users.noreply.github.com>
2024-05-08 21:02:25 +0500

    controllers new

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -160,3 +160,31 @@ func Login(c *gin.Context) {
 	}
 	c.JSON(200, tokenResponse)
 }
+func Profile(c *gin.Context) {
+	// Initialize a user model
+	var user models.User
+	// Get the email from the authorization middleware
+	email, _ := c.Get("email")
+	// Query the database for the user
+	result := database.DB.Where("email = ?", email.(string)).First(&user)
+	// If the user is not found, return a 404 status code
+	if result.Error == gorm.ErrRecordNotFound {
+		c.JSON(404, gin.H{
+			"Error": "User Not Found",
+		})
+		c.Abort()
+		return
+	}
+	// If an error occurs while retrieving the user profile, return a 500 status code
+	if result.Error != nil {
+		c.JSON(500, gin.H{
+			"Error": "Could Not Get User Profile",
+		})
+		c.Abort()
+		return
+	}
+	// Set the user's password to an empty string
+	user.Password = ""
+	// Return the user profile with a 200 status code
+	c.JSON(200, user)
+}
